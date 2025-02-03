502fe14243d755c6ba55001d77f8ab1e3c42b6c1
Naimad94 <55095623+Naimad94@users.noreply.github.com>
2025-02-03 11:40:59 -0500

    Editted Comment
    Added name and Date

--- Book.java
+++ Book.java
@@ -3,8 +3,8 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author Damian Davila
+ * @version February 3, 2025
  */
 class Book
 {
