72f972163954b5b7de5df030702c3ee14f1929a1
Naimad94 <55095623+Naimad94@users.noreply.github.com>
2025-02-24 14:09:51 -0500

    Completed

--- Book.java
+++ Book.java
@@ -13,24 +13,28 @@
  * Yes, the objects are immutable because in order to create an object you must set (author, title and page number)
  * and once created it cannot be changed or edit.
  */
-class Book
+public class Book
 {
     // The fields.
     private String author;
     private String title;
     private int pages; //Exercise 2.85: Added a field of pages of type int.
+    private int borrowed;
     private String refNumber; //Exercise 2.88: Added a field of refNumber of type String.
+    private boolean courseText; //Exercise 2.92 Added field of courseText of tyope boolean.
 
     /**
      * Set the author, title fields and pages when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle, int bookPages)
+    public Book(String bookAuthor, String bookTitle, int bookPages, boolean courseText)
     {
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages; //Exercise 2.85: Added pages into constructor.
         refNumber = ""; //Exercise 2.88 Build the constructor for refNumber.
+        borrowed = 0;
+        courseText = false; //Exercise 2.92
     }
     
     //Exercise 2.83: accessor method getAuthor() will return author.
@@ -57,6 +61,16 @@ class Book
         return pages;
     }
     
+    public int getBorrowed()
+    {
+        return borrowed;
+    }
+    
+    public boolean isCourseText()
+    {
+        return courseText;
+    }
+    
     //Exercise 2.84: Added mutator method printAuthor() that should print out author in the terrminal window.
     public void printAuthor()
     {
@@ -78,29 +92,34 @@ class Book
     public void printDetails()
     {
         if(refNumber.length() > 0)
-        {
-            print refNumber;    
+        { 
+            System.out.println("Title: " + title + ", " + "Author: " + author + ", " + "Pages: " + pages + ", "  +
+            "refNumber: " + refNumber + " Borrowed: " + borrowed);
         }
         else
         {
-            print "ZZZ"
+            System.out.println("Title: " + title + ", " + "Author: " + author + ", " + "Pages: " + pages + ", "  +
+            "refNumber: " + "ZZZ" + " Borrowed: " + borrowed);
         }
-        
-        System.out.println("Title: " + title + ", " + "Author: " + author + ", " + "Pages: " + pages + ", "  +
-        "refNumber: " + refNumber);
     }
     
     //Exercise 2.88 Created mutator method for setRefNumber that will assign the string to refNumber.
     public void setRefNumber(String ref)
     {
-        refNumber = ref;
-        
-        if(refNumber >= 3)
+        if(ref.length() >= 3) //if String is greater then or equal to 3.
         {
-            //print;
-            else
-            //System.out.println("refNumber must be atleast 3 characters long");
+            refNumber = ref; //Assigning string to refNumber
         }
+        else
+        {
+            System.out.println("refNumber must be atleast 3 characters long");//else print this error message.
+        }
+    }
+    
+    //Exercise 2.91 This method will increment the number of times book has been borrowed.
+    public void setBorrow()
+    {
+        borrowed = borrowed + 1;
     }
 
     // Add the methods here ...
